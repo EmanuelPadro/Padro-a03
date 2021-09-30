c3edd4133b1fcf590c63128b0aa752acbe207083
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-09-30 13:25:39 -0400

    Pseudo code 24

--- exercise24/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-<
-build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-V
-&src/main/java/solution/Solution24.java,2\f\2f8e986e80795567398441c250c7de2af1950a9c
---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,15 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Emanuel_Padro
+ */
+
+
+//This program will either in one or two classes check 2 words to see if they are an anagram
+//it will get input from user then send that input to be solved using isAnagram and then output an out statement
+public class Solution24 {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
