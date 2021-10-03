fce5eeec2c3303fa7498786d006991a96d961f2e
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-10-03 19:43:17 -0400

    Exercise is done

--- exercise27/src/main/java/baseline/ex27Database.java
+++ exercise27/src/main/java/baseline/ex27Database.java
@@ -1,11 +1,15 @@
 package baseline;
 
+
+
 public class ex27Database {
+    //Will set variables here
     String FirstName;
     String LastName;
     String Zip_Code;
     String ID;
 
+    //Constructor will set the callable variables with the correct data it takes in
     ex27Database(String InputFirstName,String InputLastName,String InputZipCode,String InputEmployeeID){
         FirstName = InputFirstName;
         LastName = InputLastName;
