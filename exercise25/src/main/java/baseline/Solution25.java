a1d2148c48cc7e635a23cdc1c37c97678c5e5d06
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-10-03 19:17:43 -0400

    Exercise 25 is complete

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -20,8 +20,70 @@ kinda dumb but i don't make the exercises
 if it does not comply with these categories then its unknown strength
 */
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution25 {
+    public static int passwordValidator(String password){
+        //converts password to pass array and sets traits
+        char[] pass = password.toCharArray();
+        boolean numonly = true;
+        boolean charonly = true;
+        boolean specialchar = false;
+
+        //checks to see if entire password is numbers or characters
+        for(char c : pass){
+            if(Character.isDigit(c)){
+                charonly = false;
+            }else{
+                numonly = false;
+            }
+        }
+        //Checks for special characters using matcher
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
+        Matcher matcher = pattern.matcher(password);
+        boolean containsSpecialCharacters = matcher.find();
+        if(containsSpecialCharacters){
+            specialchar = true;
+        }
+
+        //Below a score is assigned to password depending on the traits we've defined above
+        if(password.length() < 8 && numonly){
+            return -2;
+        }
+        if(password.length() < 8 && charonly){
+            return -1;
+        }
+        if(password.length() >= 8 && !charonly && !numonly && !specialchar ){
+            return 1;
+        }
+        if(password.length() >= 8 && !charonly && !numonly){
+            return 2;
+        }
+        return 0;
+    }
+
+    //Output takes returned value from validator and generates correct output
+    public static void output(int passwordValidator,String password){
+        String judgement = switch (passwordValidator) {
+            case -2 -> "very weak";
+            case -1 -> "weak";
+            case 0 -> "uncategorized 8+ character or number only";
+            case 1 -> "strong";
+            case 2 -> "very strong";
+            default -> "unrecognizable";
+        };
+        System.out.println("The password " + password + " is a " + judgement + " password");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Testing");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a password");
+        String password = input.nextLine();//Main Scans for Input and transfers data between functions
+
+        int passwordValidator = Solution25.passwordValidator(password);
+
+        output(passwordValidator,password);
     }
 }
--- exercise25/src/test/java/Solution25Test.java
+++ exercise25/src/test/java/Solution25Test.java
@@ -1,8 +1,10 @@
+import baseline.Solution25;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Solution25Test {
     @Test
     public void Test1(){
-
+        Assertions.assertEquals(Solution25.passwordValidator("Dragon"), -1);
     }
 }
