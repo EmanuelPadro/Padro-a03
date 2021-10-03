440f66a33ab929fdecf45dbb23e4901d14f75805
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-10-03 19:56:02 -0400

    Solution 29 complete and ran

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -1,21 +1,47 @@
 package baseline;
 
-/*
- *  UCF COP3330 Fall 2021 Assignment 3 Solutions
- *  Copyright 2021 Emanuel_Padro
- */
-
+import java.util.Scanner;
 
 public class Solution29 {
+    public static double Input(){
+        Scanner input = new Scanner(System.in);
+        //settin rate of return
+        double r = 72;
+        //this boolean value will be used to test validity of input
+        boolean valid;
+        do {
+            // at the beggining of each loop set both of these as true unless proven false in error cehckers bellow
+            boolean doublecheck = true;
+            valid = true;
+            System.out.println("What is the rate of return");
+            String userinput = input.nextLine();
 
-    //This method gets user input and uses a loop to make sure its aprpriate data then when it is sends it to solver
-    public static void getInput(){
+            try {
+                Double.parseDouble(userinput);
+            } catch (NumberFormatException e) {
+                doublecheck = false;
+            }
 
+            if (doublecheck) {
+                r = Double.parseDouble(userinput);
+                if (r == 0) {
+                    valid = false;
+                }
+            }else{
+                valid = false;
+            }
+
+            if (!valid) {
+                System.out.println("Sorry. That's not valid input");
+            }
+        }while(!valid);
+        return 72/r; //finnaly return value if it gets trough all these checks
     }
 
-    //main is main
     public static void main(String[] args) {
-        getInput();
-        Solver.Print();
+
+        double years = Solution29.Input();
+        System.out.print(years);
+
     }
 }
