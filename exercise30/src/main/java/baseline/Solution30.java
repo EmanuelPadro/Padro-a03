40ea3c3e75b76e230589e48f898e1e9252d405c3
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-10-03 20:00:31 -0400

    exercise 30 Upload and ran

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -6,16 +6,29 @@ package baseline;
  */
 
 
-//mult table
-
+//Will set the parameters I believe using regex or another text formatting function so that it prints table
+//will use a loop to do so( if it is too much for one
+// method then will create a second method that prepares the setup of the table and another that prints it
 public class Solution30 {
+    public static void MultiplicationTable() {
+        for (int i = 1; i <= 12; i++) {// for loop prints 12 nums and  prints them vertically
+            for (int j = 1; j <= 12; j++) {
 
-    static void printer(){
-        //Will set the parameters I believe using regex or another text formatting function so that it prints table
-        //will use a loop to do so( if it is too much for one
-        // method then will create a second method that prepares the setup of the table and another that prints it
+                //These statemets take care of some spacing issues
+                if (i * j >= 100) {
+                    System.out.print(" " + i * j);
+                } else if (i * j >= 10) {
+                    System.out.print("  " + i * j);
+                } else if (i * j < 10) {
+                    System.out.print("   " + i * j);
+                }
+            }
+            System.out.println();
+        }
     }
-    //main is main
+
+
     public static void main(String[] args) {
+        MultiplicationTable();
     }
 }
